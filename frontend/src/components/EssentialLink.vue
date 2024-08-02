4895f53a0ffaf1ab5fed20bd98be64db4bb9a7ba
tmeftah <sudtechnics@gmail.com>
2024-08-02 11:29:42 +0200

    reorder frontend folder

--- frontend/src/components/EssentialLink.vue
+++ frontend/src/components/EssentialLink.vue
@@ -1,12 +1,13 @@
 <template>
-  <q-item clickable tag="a" :href="props.link">
+
+  <q-item clickable tag="a" :href="props.link" class="q-py-md" >
     <q-item-section v-if="props.icon" avatar>
       <q-icon :name="props.icon" />
     </q-item-section>
 
     <q-item-section>
-      <q-item-label>{{ props.title }}</q-item-label>
-      <q-item-label caption>{{ props.caption }}</q-item-label>
+      <q-item-label class="text-grey-8">{{ props.title }}</q-item-label>
+      <!-- <q-item-label caption>{{ props.caption }}</q-item-label> -->
     </q-item-section>
   </q-item>
 </template>
--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+  <q-layout view="FHh Lpr lFf">
+    <q-header dark bordered class="bg-grey-1 text-grey-8">
       <q-toolbar>
         <q-btn
           flat
@@ -10,14 +10,31 @@
           aria-label="Menu"
           @click="toggleLeftDrawer"
         />
+        <q-img
+          src="https://suite.knowron.com/_next/image?url=https%3A%2F%2Fd2ccnoziqedwbs.cloudfront.net%2Flogos%2Flogo-kardex-group.png&w=256&q=75"
+          width="140px"
+        />
 
-        <q-toolbar-title> RAGGI </q-toolbar-title>
+
+        <q-toolbar-title>  RAGGI </q-toolbar-title>
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      :mini="miniState"
+      @mouseover="miniState = false"
+      @mouseout="miniState = true"
+      width="220"
+    >
+
       <q-list>
-        <q-item-label header> Menu </q-item-label>
+        <q-img
+          src="https://suite.knowron.com/_next/image?url=https%3A%2F%2Fd2ccnoziqedwbs.cloudfront.net%2Flogos%2Flogo-kardex-group.png&w=256&q=75"
+          width="140px"
+        />
 
         <EssentialLink
           v-for="link in linksList"
@@ -45,18 +62,25 @@ const linksList = [
   {
     title: "Dashboard",
     caption: "overview",
-    icon: "school",
+    icon: "line_axis",
     link: "1",
   },
   {
     title: "Ask a question",
     caption: "chat with docs",
-    icon: "code",
+    icon: "chat_bubble_outline",
     link: "2",
+  },
+  {
+    title: "Alle Dokumente",
+    caption: "chat with docs",
+    icon: "folder_open",
+    link: "/",
   },
 ];
 
 const leftDrawerOpen = ref(false);
+const miniState = ref(true);
 
 function toggleLeftDrawer() {
   leftDrawerOpen.value = !leftDrawerOpen.value;
