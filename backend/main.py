1d1a0e8920c07de2fb8410e6fe83e12feb3628d9
NikhilRaikar17 <nikhilraikar88@gmail.com>
2024-08-04 16:22:25 +0200

    Added health router and also updated the guthub workflows

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from backend.api.health import health_router 
 from backend.api.user import user_router
 from backend.api.token import token_router
 from backend.api.query import query_router
@@ -17,10 +18,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.include_router(health_router)
 app.include_router(token_router)
 app.include_router(user_router)
 app.include_router(query_router)
 populate_admin_user()
-
-
-
