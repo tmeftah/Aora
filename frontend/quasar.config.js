6ec178bf7a15968b39b4d412db45d56944e2b63a
tmeftah <sudtechnics@gmail.com>
2024-08-12 11:30:50 +0200

    add notify module

--- frontend/quasar.config.js
+++ frontend/quasar.config.js
@@ -100,7 +100,11 @@ module.exports = configure(function (/* ctx */) {
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#framework
     framework: {
-      config: {},
+      config: {
+        notify: {
+          // Notify defaults
+        },
+      },
 
       // iconSet: 'material-icons', // Quasar icon set
       // lang: 'en-US', // Quasar language pack
@@ -113,7 +117,7 @@ module.exports = configure(function (/* ctx */) {
       // directives: [],
 
       // Quasar plugins
-      plugins: [],
+      plugins: ["Notify"],
     },
 
     // animations: 'all', // --- includes all animations
