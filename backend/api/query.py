4438b9f5a1069b90bb6ca595b649f2161156b0cf
raikarn <nikhil.raikar@apl-landau.de>
2024-08-23 16:07:02 +0200

    Added auth to query service

--- backend/api/query.py
+++ backend/api/query.py
@@ -1,24 +1,41 @@
 from fastapi import APIRouter
+from fastapi import Depends
 from fastapi import HTTPException
+from sqlalchemy.orm import Session
+
+from backend.db.sessions import get_db
+from backend.exceptions import NoValidPermissionsException
+from backend.models.sqlalchemy_models import User
+from backend.service.oauth import get_current_user
 from backend.service.query_service import query_service
 
-query_router = APIRouter(prefix="/query",
-                         tags=["Query"],
-                         responses={
-                             200: {"description": "Success"},
-                             404: {"description": "Resource Not Found"},
-                             500: {"description": "Internal Server Error"},
-                         },)
+query_router = APIRouter(
+    prefix="/query",
+    tags=["Query"],
+    responses={
+        200: {"description": "Success"},
+        404: {"description": "Resource Not Found"},
+        500: {"description": "Internal Server Error"},
+    },
+)
 
 
 @query_router.get("/")
-async def query(query: str):
+async def query(
+    query: str,
+    current_user: User = Depends(get_current_user),
+    db: Session = Depends(get_db),
+):
     """
     Handle query requests from user and
     return appropriate response
     """
     try:
-        response = await query_service(query)
+        response = await query_service(
+            query=query, current_user=current_user, db=db
+        )
         return response
+    except NoValidPermissionsException as e:
+        raise HTTPException(status_code=403, detail=str(e))
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
--- backend/service/query_service.py
+++ backend/service/query_service.py
@@ -1,15 +1,25 @@
 import json
 import uuid
+
 from fastapi.responses import StreamingResponse
+from sqlalchemy.orm import Session
 
-from backend.rag_llms_langchain import chain
 from backend.embeddings.ingest import get_vectorstore
+from backend.exceptions import NoValidPermissionsException
+from backend.models.sqlalchemy_models import User
+from backend.rag_llms_langchain import chain
+from backend.service.oauth import check_current_user_permissions
 
 
-async def query_service(query: str):
+async def query_service(query: str, current_user: User, db: Session):
     """
     Process a query and return a streaming response.
     """
+    user_has_permissions = check_current_user_permissions(
+        current_user=current_user, db=db
+    )
+    if not user_has_permissions:
+        raise NoValidPermissionsException()
 
     store = get_vectorstore()
     docs = store.invoke(query)
@@ -23,5 +33,6 @@ async def query_service(query: str):
 
         # TODO  here we have to add the metadata/source
 
-    return StreamingResponse(stream_generator(),
-                             media_type="application/x-ndjson")
+    return StreamingResponse(
+        stream_generator(), media_type="application/x-ndjson"
+    )
