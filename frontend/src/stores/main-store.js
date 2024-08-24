223d530bf1ef955c9f18ff91fa667e00ac1a1387
tmeftah <sudtechnics@gmail.com>
2024-08-24 19:38:08 +0200

    feat ✨: Improved user interface
    - Improved the user interface by adding a select dropdown to filter models.
    
    - Addthe ability to send a question along with the model name when pressing enter.

--- frontend/src/stores/main-store.js
+++ frontend/src/stores/main-store.js
@@ -1,5 +1,9 @@
 import { defineStore } from "pinia";
 
+import { useAuthStore } from "stores/auth";
+
+const authStore = useAuthStore();
+
 const baseUrl = `${process.env.API}`;
 
 export const useMainStore = defineStore("main", {
@@ -7,14 +11,77 @@ export const useMainStore = defineStore("main", {
     loading: false,
     question: "",
     solution: "",
+    models: JSON.parse(localStorage.getItem("models")) || [],
+    model_name: localStorage.getItem("model_name") || "",
   }),
   getters: {},
 
   actions: {
-    async sendQA(question) {
+    async get_models() {
+      try {
+        const response = await fetch(`${baseUrl}/query/list_models`, {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${authStore.token}`,
+          },
+        });
+
+        if (!response.ok) {
+          // Extract error message if available
+          const errorData = await response.json().catch(() => ({})); // Parsing might fail, default to empty object
+          const errorMessage =
+            errorData.message || `Error: ${response.statusText}`;
+          throw new Error(errorMessage);
+        }
+
+        const data = await response.json();
+        this.models = data;
+        localStorage.setItem("models", JSON.stringify(this.models));
+
+        //
+      } catch (error) {
+        // Handle network errors and HTTP errors
+        if (error.name === "TypeError") {
+          // This typically indicates a network error
+          console.error("Network error: Could not reach the server");
+          Notify.create({
+            color: "negative",
+            position: "bottom",
+            message: error.message,
+            icon: "report_problem",
+          });
+        } else {
+          // HTTP error, or some other error
+          console.error(`API error: ${error.message}`);
+          Notify.create({
+            color: "negative",
+            position: "bottom",
+            message: error.message,
+            icon: "report_problem",
+          });
+        }
+
+        // You can rethrow the error or handle it in some way, e.g., user notification
+      }
+    },
+
+    set_model_name(name) {
+      localStorage.setItem("model_name", name);
+    },
+
+    async sendQA(question, model_name) {
       this.solution = "";
       this.loading = true;
-      await fetch(`${baseUrl}/query?query=${question}`)
+      await fetch(
+        `${baseUrl}/query?query=${question}&model_name=${model_name}`,
+        {
+          headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${authStore.token}`,
+          },
+        }
+      )
         .then((response) => {
           console.log(response.body);
           if (!response.ok) {
