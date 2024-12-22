f82fc0b43d5302f1c98c405553936bb94607e98c
NikhilRaikar17 <nikhilraikar88@gmail.com>
2024-12-22 17:42:42 +0100

    Added post method in topics

--- backend/api/topics.py
+++ backend/api/topics.py
@@ -6,6 +6,8 @@ from sqlalchemy.orm import Session
 from backend.db.sessions import get_db
 from backend.service.oauth import get_current_user
 from backend.service.topics_service import get_all_topics
+from backend.service.topics_service import create_topic
+from backend.exceptions import DuplicateUserException
 
 
 topic_router = APIRouter(
@@ -31,5 +33,28 @@ async def get_topics(
         raise HTTPException(
             status_code=500,
             detail=str(e),
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+@topic_router.post("/", dependencies=[Depends(get_current_user)])
+async def create_topics(
+    name: str,
+    db: Session = Depends(get_db),
+):
+    """Create a new topic"""
+    try:
+        return create_topic(
+            db=db,
+            name=name,
+        )
+
+    except DuplicateUserException as e:
+        raise HTTPException(status_code=403, detail=str(e))
+
+    except Exception as e:
+        print(e)
+        raise HTTPException(
+            status_code=500,
+            detail="Internal Server error",
             headers={"WWW-Authenticate": "Bearer"},
         )
