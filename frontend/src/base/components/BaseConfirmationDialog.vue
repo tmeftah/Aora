3ef343bc7878e6db98ae996b3385b0ef765a7d02
nikhilraikar <nikhil.raikar@apl-landau.de>
2025-03-08 20:27:26 +0100

    Updated baseconfirmation dialog and added more consistency to all dialog boxes

--- frontend/src/base/components/BaseConfirmationDialog.vue
+++ frontend/src/base/components/BaseConfirmationDialog.vue
@@ -38,19 +38,23 @@ function closeDialog() {
 
 <template>
   <q-dialog id="confirmation-dialog" v-model="internalDialog" persistent @update:modelValue="updateDialog">
-    <q-card class="q-pa-md" style="min-width: 400px">
-      <q-card-section>
-        <div class="text-h6" id="dialog-title">{{ title }}</div>
+    <q-card style="width: 700px; max-width: 80vw;">
+      <q-card-section class="row items-center text-white" style="background-color: #075070;">
+        <q-icon name="delete" icon="delete" color="red" size="2rem" class="q-mr-sm" />
+        <div class="text-h5 text-bold" id="dialog-title">{{ title }}</div>
+        <q-space />
+        <q-btn icon="close" flat round dense v-close-popup />
       </q-card-section>
 
       <q-separator />
 
-      <q-card-section>
-        <span v-html="text" id="dialog-text"></span>
+      <q-card-section style="max-height: 50vh">
+        <div class="text-h7" v-html="text" id="dialog-text"></div>
       </q-card-section>
 
       <q-card-actions align="right">
-        <q-btn label="Cancel" color="blue-grey" outline class="text-white" id="cancel-button" @click="closeDialog" />
+        <q-btn label="Cancel" outline class="text-black" id="cancel-button" @click="closeDialog" style="color: #075070;"
+          v-close-popup />
         <q-btn label="Delete" color="red" class="text-white" id="delete-button" @click="$emit('deleteElement')" />
       </q-card-actions>
     </q-card>
